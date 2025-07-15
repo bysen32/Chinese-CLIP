1a6e294e0b1c8a62dd0f78a550863be938eee87e
bysen32 <bysen32@gmail.com>
2025-07-15 11:28:05 +0800

    check data processing script for building LMDB dataset

--- cn_clip/preprocess/build_lmdb_dataset.py
+++ cn_clip/preprocess/build_lmdb_dataset.py
@@ -13,6 +13,24 @@ import json
 import pickle
 
 
+'''
+${split}_texts.jsonl
+文本信息及图文对匹配关系保存在 ${split}_texts.jsonl 文件中，格式如下：
+{
+    'text_id': 'text_id_1',
+    'text': 'This is a text annotation.',
+    'image_ids': ['image_id_1', 'image_id_2']
+}
+
+${split}_imgs.tsv
+为提升文件处理效率，将图片以base64形式分别存放在 ${split}_imgs.tsv 文件中。
+每行表示一张图片，包含图片id 与 图片 base64，以tab隔开
+1000002	/9j/4AAQSkZJ...YQj7314oA//2Q==
+
+最后将 tsv 和 jsonl 文件一起序列化，
+转换为内存索引的LMDB数据库文件，方便训练时随机读取
+'''
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -40,15 +58,18 @@ if __name__ == "__main__":
     # build LMDB data files
     if args.lmdb_dir is None:
         args.lmdb_dir = os.path.join(args.data_dir, "lmdb")
+
     for split in specified_splits:
         # open new LMDB files
         lmdb_split_dir = os.path.join(args.lmdb_dir, split)
         if os.path.isdir(lmdb_split_dir):
             print("We will overwrite an existing LMDB file {}".format(lmdb_split_dir))
         os.makedirs(lmdb_split_dir, exist_ok=True)
+
         lmdb_img = os.path.join(lmdb_split_dir, "imgs")
         env_img = lmdb.open(lmdb_img, map_size=1024**4)
         txn_img = env_img.begin(write=True)
+
         lmdb_pairs = os.path.join(lmdb_split_dir, "pairs")
         env_pairs = lmdb.open(lmdb_pairs, map_size=1024**4)
         txn_pairs = env_pairs.begin(write=True)
@@ -60,9 +81,11 @@ if __name__ == "__main__":
             for line in tqdm(fin_pairs):
                 line = line.strip()
                 obj = json.loads(line)
+                # Check valid field
                 for field in ("text_id", "text", "image_ids"):
                     assert field in obj, "Field {} does not exist in line {}. \
                         Please check the integrity of the text annotation Jsonl file."
+
                 for image_id in obj["image_ids"]:
                     dump = pickle.dumps((image_id, obj['text_id'], obj['text'])) # encoded (image_id, text_id, text)
                     txn_pairs.put(key="{}".format(write_idx).encode('utf-8'), value=dump)  
@@ -70,8 +93,9 @@ if __name__ == "__main__":
                     if write_idx % 5000 == 0:
                         txn_pairs.commit()
                         txn_pairs = env_pairs.begin(write=True)
+            
             txn_pairs.put(key=b'num_samples',
-                    value="{}".format(write_idx).encode('utf-8'))
+                          value="{}".format(write_idx).encode('utf-8'))
             txn_pairs.commit()
             env_pairs.close()
         print("Finished serializing {} {} split pairs into {}.".format(write_idx, split, lmdb_pairs))
@@ -89,7 +113,7 @@ if __name__ == "__main__":
                     txn_img.commit()
                     txn_img = env_img.begin(write=True)
             txn_img.put(key=b'num_images',
-                    value="{}".format(write_idx).encode('utf-8'))
+                        value="{}".format(write_idx).encode('utf-8'))
             txn_img.commit()
             env_img.close()                
         print("Finished serializing {} {} split images into {}.".format(write_idx, split, lmdb_img))
